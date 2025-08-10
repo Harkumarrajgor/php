bb299c62041e5e2bdd4741e301b6b7b08389ac6f
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-10 23:38:47 +0530

    Add files via upload

---
+++ u2-6.php
@@ -0,0 +1,10 @@
+<?php
+	$my=array(12,45,4,5,82);
+	$min=min($my);
+	$max=max($my);
+	
+	echo "Array elements".implode(",",$my);
+	
+	echo "<br>maximum value:$max<br>";
+	echo "minimum value:$min";
+?>
