2fc69992f7de7d46c80534f766b11ace246a291b
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-27 23:26:17 +0530

    Add files via upload

---
+++ u2-11.php
@@ -0,0 +1,13 @@
+<?php
+	$student = array(
+		"Name"   => "Aarya",
+		"Age"    => 19,
+		"Course" => "B.Sc Nursing",
+		"City"   => "Ahmedabad"
+	);
+
+	echo "Student name     ".$student["Name"]."<br>";
+	echo "Age".$student["Age"]."<br>";
+	echo "Course".$student["Course"]."<br>";
+	echo "City".$student["City"]."<br>";
+?>
